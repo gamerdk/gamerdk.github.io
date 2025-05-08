699124371e2aa2b3003716acdfb0edda1c7bcf24
Daniël Kosters <0356076@student.rocvantwente.nl>
2025-05-08 10:27:46 +0200

    fix
    bestanden worden nu fatsoenlijk gevonden

--- bpv-verslag/script.js
+++ bpv-verslag/script.js
@@ -1,6 +1,6 @@
 function AddFrontPage(pageType, extraHTML = "") {
     frontPageString = `
-    <img class="position-absolute end-0" id="logo" src="/Images/Curious-Logo.webp" alt="curious-inc logo"> 
+    <img class="position-absolute end-0" id="logo" src="/bpv-verslag/Images/Curious-Logo.webp" alt="curious-inc logo"> 
     <div class="frond-page d-flex align-items-center justify-content-center">
         <div class="row-cols text-center">
             <h1 class="col p-1">Curious inc.</h1>
@@ -36,7 +36,7 @@ function FillHead() {
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
     crossorigin="anonymous"></script>
-  <link rel="stylesheet" href="/style.css">
+  <link rel="stylesheet" href="/bpv-verslag/style.css">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   `;
     const header = document.querySelector("head");
